8cb13134009aa9f689704d383ed184bfc32ef07a
Taylor Klodowski <taylor@theskimm.com>
2021-03-19 18:20:08 -0400

    Update fetch headers

--- pages/index.js
+++ pages/index.js
@@ -94,7 +94,7 @@ export default function MediaPlayer({ tracksById }) {
 
 export async function getStaticProps() {
   const res = await fetch(
-    'https://s3-us-west-2.amazonaws.com/anchor-website/challenges/bsb.json'
+    'https://s3-us-west-2.amazonaws.com/anchor-website/challenges/bsb.json',
     {
       headers: {
         'Content-Security-Policy': 'upgrade-insecure-requests',
